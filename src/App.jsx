dfa9bc43389651b60a06747b440fbfad3c5ad88a
Rajput-Developer <ahsanrehman728@gmail.com>
2025-01-15 19:15:23 -0800

    some-updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 
 import Navbar from './components/navbar/Navbar'
-import { Outlet } from 'react-router-dom';
-import './App.css'
+import { lazy,Suspense } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home.jsx'
+const CatMovies = lazy(() => import('./components/MoviesCat/CatMoviesService/CatMovies.jsx'))
+const About = lazy(() => import('./components/About/About.jsx'));
+const DetailPage = lazy(() => import('./components/DetailPage/DetailPage.jsx'))
+const Wishlist = lazy(() => import('./components/Wishlist/Wishlist.jsx'))
+const Bookmarked = lazy(() => import('./components/Bookmarks/Bookmarked.jsx'))
 import Footer from './components/Footer/Footer';
+import './App.css'
 
 function App() {
 
@@ -10,6 +17,15 @@ function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search/:name' element={<Home />} />
+        <Route path='/about' element={<Suspense> <About /></Suspense>} />
+        <Route path='/movies/:name' element={<Suspense><DetailPage /></Suspense>} />
+        <Route path='/like' element={<Suspense><Wishlist /></Suspense>} />
+        <Route path='/bookmarks-movies' element={<Suspense><Bookmarked /></Suspense>} />
+        <Route path='/catageory/:name' element={<Suspense><CatMovies /></Suspense>} />
+      </Routes>
       <Outlet />
       <Footer />
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,60 +1,21 @@
-import { lazy, StrictMode, Suspense } from 'react'
+import { StrictMode } from 'react'
 import App from './App.jsx'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 import { sentralStore, presistor } from './Store/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import Home from './components/Home/Home.jsx'
-const CatMovies = lazy(() => import('./components/MoviesCat/CatMoviesService/CatMovies.jsx'))
-const About = lazy(() => import('./components/About/About.jsx'));
-const DetailPage = lazy(() => import('./components/DetailPage/DetailPage.jsx'))
-const Wishlist = lazy(() => import('./components/Wishlist/Wishlist.jsx'))
-const Bookmarked = lazy(() => import('./components/Bookmarks/Bookmarked.jsx'))
+import { ToastContainer } from 'react-toastify';
 
-// Defining the Routes
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: '/search/:name',
-        element: <Home />
-      },
-      {
-        path: '/about',
-        element: <Suspense> <About /></Suspense>
-      },
-      {
-        path: '/movies/:name',
-        element: <Suspense><DetailPage /></Suspense>
-      },
-      {
-        path: '/like',
-        element: <Suspense><Wishlist /></Suspense>
-      },
-      {
-        path: '/bookmarks-movies',
-        element: <Suspense><Bookmarked /></Suspense>
-      },
-      {
-        path: '/catageory/:name',
-        element: <Suspense><CatMovies /></Suspense>
-      }
-    ]
-  },
-])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={sentralStore}>
-      <PersistGate loading={null} persistor={presistor}>
-        <RouterProvider router={router} />
-      </PersistGate>
-    </Provider>
-  </StrictMode>,
+    <BrowserRouter>
+        <Provider store={sentralStore}>
+          <PersistGate loading={null} persistor={presistor}>
+            <App />
+          </PersistGate>
+        </Provider>
+      <ToastContainer />
+    </BrowserRouter>
+  </StrictMode>
 )
